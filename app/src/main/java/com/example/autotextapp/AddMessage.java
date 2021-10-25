c010008c5a79ad757e44def9723f7456a95f1fe4
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-10-25 00:16:10 -0600

    added code to add a message and phone numbers to the app.

---
+++ app/src/main/java/com/example/autotextapp/AddMessage.java
@@ -0,0 +1,14 @@
+package com.example.autotextapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_message);
+    }
+}
