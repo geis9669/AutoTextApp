ae260b265ee460fcdf3d0b5b3dec9c0b580903ff
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-10-27 16:51:43 -0600

    added an onoptionsitmesselected method

--- app/src/main/java/com/example/autotextapp/AddMessage.java
+++ app/src/main/java/com/example/autotextapp/AddMessage.java
@@ -3,6 +3,9 @@ package com.example.autotextapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class AddMessage extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class AddMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_message);
+        Toolbar toolbaradd = findViewById(R.id.toolbaradd);
+        setSupportActionBar(toolbaradd);
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // put the code for the item menu and return.
+        if(item.getItemId() == R.id.action_save)
+        {
+
+            // save the info.
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
