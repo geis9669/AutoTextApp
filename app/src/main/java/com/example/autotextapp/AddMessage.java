4ecdf419b549a276423bff172fde576d04128198
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-10-27 17:57:36 -0600

    added menu items

--- app/src/main/java/com/example/autotextapp/AddMessage.java
+++ app/src/main/java/com/example/autotextapp/AddMessage.java
@@ -1,19 +1,34 @@
 package com.example.autotextapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 
 public class AddMessage extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_message);
+        
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_message_toolbar, menu);
+        return true;
     }
 
     public boolean onOptionsItemSelected(MenuItem item)
