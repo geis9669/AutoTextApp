0aeca78f4187c4612da1ccfa534b159fa101c02c
geis9669 <42647833+geis9669@users.noreply.github.com>
2021-11-24 22:34:14 -0700

    added class to hold data about messages and associated phone numbers

---
+++ app/src/main/java/com/example/autotextapp/data/PhoneMessageSendList.java
@@ -0,0 +1,79 @@
+package com.example.autotextapp.data;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneMessageSendList {
+    private Map<String, String[]> messagePhoneList;
+    // either have this class hold all my data
+    // or have it hold one instance of data and a list in
+    // Main activity will hold the rest.
+
+    public PhoneMessageSendList()
+    {
+        messagePhoneList = new Hashtable<>();
+
+    }
+
+    public void addMessage_PhoneNumbers(String message, ArrayList<String> phoneNumbers)
+    {
+        String[] phoneNumbersArray = new String[phoneNumbers.size()];
+        phoneNumbers.toArray(phoneNumbersArray);
+        messagePhoneList.put(message, phoneNumbersArray);
+    }
+
+    public void addMessage_PhoneNumbers(String message, String[] phoneNumbers)
+    {
+        messagePhoneList.put(message, phoneNumbers);
+    }
+
+    public void removeMessage_PhoneNumbers(String messageToRemove)
+    {
+        messagePhoneList.remove(messageToRemove);
+    }
+
+    /**
+     * Gets the message associated with the phoneNumber
+     * or null if no message is associated with the phoneNumber.
+     * @param phoneNumber the phone number to search for
+     * @return the message associated with the phoneNumber, or null.
+     */
+    public String getMessage(String phoneNumber)
+    {
+        Set<String> messageList = messagePhoneList.keySet();
+        for(String message : messageList)
+        {
+            String[] phoneNums = messagePhoneList.get(message);
+            if(phoneNums != null)
+            {
+                //could sort the list when its added then use binary search.
+                for(String phoneNum : phoneNums)
+                {
+                    if(phoneNumber.equals(phoneNum))
+                    {
+                        return message;
+                    }
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public Set<String> getMessages()
+    {
+        return messagePhoneList.keySet();
+    }
+
+    public String[] getPhoneNumbers(String message)
+    {
+        return messagePhoneList.get(message);
+    }
+
+    public int size()
+    {
+        return messagePhoneList.size();
+    }
+}
